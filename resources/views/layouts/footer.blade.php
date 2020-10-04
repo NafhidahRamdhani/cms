3f41fb553ce63a64114c1079bd7d2b408d5af46f
Nafhii <nafhidah.ramdhani1@gmail.com>
2020-10-04 17:03:15 +0700

    KUIS NAFHIDAH RAMDHANI
    MI-2E / 1931710018

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,121 @@
+<!-- Footer -->
+<footer class="page-footer font-small warning-color lighten-1">
+
+  <div style="background-color: #000000">
+    <div class="container">
+
+      <!-- Grid row-->
+      <div class="row py-5 d-flex align-items-center">
+
+        <!-- Grid column -->
+        <div class="col-md-6 col-lg-5 text-center text-md-left mb-8 mb-md-0">
+          <h6 class="mb-0 text-warning">FOLLOW WITH US ON SOCIAL NETWORK!</h6>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-6 col-lg-7 text-center text-md-right">
+
+          <!--Linkedin -->
+          <a class="li-ic">
+            <i class="fab fa-linktree-in white-text mr-4"> </i>
+          </a>
+          <!--Instagram-->
+          <a class="ins-ic">
+            <i class="fab fa-email white-text"> </i>
+          </a>
+
+        </div>
+        <!-- Grid column -->
+
+      </div>
+      <!-- Grid row-->
+
+    </div>
+  </div>
+
+  <!-- Footer Links -->
+  <div class="container text-center text-md-left mt-5">
+
+    <!-- Grid row -->
+    <div class="row mt-3 dark-grey-text">
+
+      <!-- Grid column -->
+      <div class="col-md-3 col-lg-4 col-xl-3 mb-4">
+
+        <!-- Content -->
+        <h6 class="text-uppercase font-weight-bold">INI NAFHI</h6>
+        <hr class="teal accent-3 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+        <p>Hello Dear, you can buy some product fashion in nafhi.id, if you want to request gift for special people let's check allcustom.id. Last, when you need cleaning services or delivery stuff common check sekresek_</p>
+
+      </div>
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+      <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+
+        <!-- Links -->
+        <h6 class="text-uppercase font-weight-bold">Products</h6>
+        <hr class="teal accent-3 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+        <p>
+          <a class="dark-grey-text" href="#!">FASHION</a>
+        </p>
+        <p>
+          <a class="dark-grey-text" href="#!">GIFT</a>
+        </p>
+        <p>
+          <a class="dark-grey-text" href="#!">SERVICES</a>
+        </p>
+
+      </div>
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+      <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+
+        <!-- Links -->
+        <h6 class="text-uppercase font-weight-bold">CHECK</h6>
+        <hr class="teal accent-3 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+        <p>
+          <a class="dark-grey-text" href="#!">REAL ACCOUNT</a>
+        </p>
+        <p>
+          <a class="dark-grey-text" href="#!">TESTIMONI</a>
+        </p>
+        <p>
+          <a class="dark-grey-text" href="#!">HELP</a>
+        </p>
+
+      </div>
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+      <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+
+        <!-- Links -->
+        <h6 class="text-uppercase font-weight-bold">CONTACT</h6>
+        <hr class="teal accent-3 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+        <p>
+          <i class="fas fa-store mr-3"></i> Malang, lowokwaru, East Java</p>
+        <p>
+          <i class="fab fa-instagram mr-3"></i> @nafhidahrq_</p>
+        <p>
+          <i class="fas fa-phone mr-3"></i> 082333889670</p>
+
+      </div>
+      <!-- Grid column -->
+
+    </div>
+    <!-- Grid row -->
+
+  </div>
+  <!-- Footer Links -->
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center text-black-50 py-3">© 2020 Copyright:
+    <a class="dark-grey-text"> ININAFHI.com</a>
+  </div>
+  <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,150 @@
+<nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark">
+    <a class="navbar-brand text-warning" href="#">NAFHI's SMALL BUSINESS</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto my-2 my-lg-1">
+        <li class="nav-item active">
+          <a class="nav-link btn btn-warning text-dark" href="#">HOME <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link btn btn-warning text-dark" href="#">CONTACT ME</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link btn btn-warning text-dark" href="#" tabindex="-1" aria-disabled="true">HELP</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link btn btn-warning text-dark" href="#" tabindex="-1" data-toggle="modal" data-target="#kampret">MENU</a>
+        </li>
+        <li class="nav-item mr-2">
+          <a class="nav-link btn btn-warning text-dark" href="#" tabindex="-1" data-toggle="modal" data-target="#free">ORDER</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+
+  <!--end navbar-->
+  <!--jumbotron-->
+  <div class="jumbotron jumbotron-fluid bg-warning">
+    <div class="container">
+      <h1 class="display-3 text-center text-dark">SMALL BUSINESS</h1>
+      <p class="lead text-center text-dark">You can search product, gift, or services everythink what do you need in here dear!</p>
+      <div class="input-group mb-4">
+        <input type="text-dark" class="form-control" placeholder="What do you need dear?" aria-label="Recipient's username"
+          aria-describedby="basic-addon2">
+        <div class="input-group-append">
+          <button class="btn btn-dark text-warning" id="search">Search</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--end jumbotron-->
+
+      <!--end jumbotron-->
+    <!-- Categories -->
+    <div class="container-fluid">
+      <h1 class="display-5 text-center mb-5">Categories</h1>
+      <div class="card-deck mb-5">
+        <div class="card">
+          <img class="card-img-top img-thumbnail" src="{{ asset ('nafhi.id.png')}}" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">@nafhi.id</h5>
+            <p class="card-text">OUTFIT SELEBGRAM HARGA ANAK KOSAN</p>
+            <p class="card-text">Buat kalian para sistur yang pengen beli outfit highclass harga lowbudget BURUAN MERAPAT HYUNG</p>
+            <a href="{{ 'https://instagram.com/nafhi.id' }}" class="btn btn-warning text-dark">View Product &rarr;</a>
+          </div>
+        </div>
+        <div class="card">
+            <img class="card-img-top img-thumbnail" src="{{ asset ('all.png')}}" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">@allcustomid_</h5>
+              <p class="card-text">AESTHETHIC GIFT</p>
+              <p class="card-text">HI lur! Mau kadoin doi, temen, sahabat, keluarga tapi gatau beli dimana? YOK GASKEUN cek to thr cek</p>
+              <a href="{{ 'https://instagram.com/allcustomid_' }}" class="btn btn-warning text-dark">View More &rarr;</a>
+            </div>
+        </div>
+        <div class="card">
+            <img class="card-img-top img-thumbnail" src="{{ asset ('sek.png')}}" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">@sekresek_</h5>
+              <p class="card-text">SERVICES AT ANYTIME</p>
+              <p class="card-text">Punten akang teteh, yang punya kosan tapi ga ditempatin karena pandemi kuy pakai jasa kami, sayang duid nya atu</p>
+              <a href="{{ 'https://instagram.com/sekresek_' }}" class="btn btn-warning text-dark">View Services &rarr;</a>
+            </div>
+        </div> 
+      </div>    
+    </div> 
+    <!-- End Categories -->
+
+    <!-- End Categories -->
+    <!-- Article -->
+    <div class="container-fluid">
+      <!-- Example row of columns -->
+      <div class="row p-5 bg-dark text-warning">
+        <div class="col-md-12">
+            <h2 class="display-5 text-center mb-5">A R T I C L E</h2>
+        </div>
+        <div class="col-md-4">
+          <h2>NAFHI.ID</h2>
+          <p>@nafhi.id | Flashback 3 tahun lalu tepatnya 2016, saya mendirikan sebuah olshop kecil kecilan. Bermula posting di snap whatsapp sampe membuat grub olshop di WA. Namun pada tahun 2018 tepatnya saya udah memasuki bangku kelas 3 SMK, saya disibukkan dengan ujian demi ujian yang mana ortu menyuruh untuk break sebentar berjualan. TO BE CONTINUED</p> 
+          <p><a class="btn btn-warning" href="#" role="button">View Story &raquo;</a></p>
+        </div>
+        <div class="col-md-4">
+          <h2>ALLCUSTOMID_</h2>
+          <p>@allcustomid_ | Mulanya business ini dibangun bersama teman saya di malang. Dhea namanya. Kita punya cita cita dan tujuan yang sama, menjadi seorang entreprenuer. Sangat disayangkan pandemi membuat kita harus bersabar untuk menyiapkan sesuatu untung membangun bisnis ini. Bismillah kedepan ada jalan. aamiin
+               </p>
+          <p><a class="btn btn-warning" href="#" role="button">View Story &raquo;</a></p>
+        </div>
+        <div class="col-md-4">
+          <h2>SEKRESEK_</h2>
+          <p>@sekresek_ | Pandemi membuatku berpikir bagaimana carnya menghasilkan uang dengan cara yang halal tentunya. Kemudian saya inisiatif membuka jasa bersih2 kos, kontrakan atau asrama. Lalu ada pengosongan dan pengiriman paket. Yang baru berjalan agustus lalu. Doain lancar dan berkah ya! Aamiin Allahumaa Aamiin</p>
+          <p><a class="btn btn-warning" href="#" role="button">View Services &raquo;</a></p>
+        </div>
+      </div>
+      <hr>
+    </div>
+    <!-- End Article -->   
+
+     <!-- End Article -->
+    <!-- Top Author -->
+  <section class="bg-warning" id="team">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-lg-12 text-dark">
+          <h2 class="section-heading text-uppercase">OWNER</h2>
+          <h3 class="section-subheading text-muted">@nafhi.id @allcustomid_ @sekresek_</h3>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-sm-4">
+          <div class="team-member">
+            <img class="mx-auto rounded-circle tayo" src="{{ asset ('tayo.jpg')}}" alt="">
+            <h4>NAFHIDAH RAMDHANI QURRAHMAN</h4>
+            <p class="text-muted">Mahasiswa Politeknik Negeri Malang</p>
+            <ul class="list-inline social-buttons">
+              <li class="list-inline-item">
+                <a href="#">
+                  <i class="fab fa-twitter"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a href="#">
+                  <i class="fab fa-facebook-f"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a href="#">
+                  <i class="fab fa-linkedin-in"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+    <!-- End Top Author -->
